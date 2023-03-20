417cd31339a2cde91641f5803e195a0a3eceb0f5
@MusaWiga2022 <musalordkai@gmail.com>
2023-03-20 08:20:24 -0700

    rullupConfigured

--- rollup.config.js
+++ rollup.config.js
@@ -44,10 +44,9 @@ export default {
 				dev: !production
 			}
 		}),
+		css({output: 'bundle.css'}),
 		// we'll extract any component CSS out into
 		// a separate file - better for performance
-		css({ output: 'bundle.css' }),
-
 		// If you have external dependencies installed from
 		// npm, you'll most likely need these plugins. In
 		// some cases you'll need additional configuration -
--- src/main.js
+++ src/main.js
@@ -1,8 +1,13 @@
 import App from './App.svelte';
+import 'blueprint-css/dist/blueprint.min.css'
 
 const app = new App({
-	target: document.body,
+	target: document.body
 
 });
 
 export default app;
+
+
+
+
